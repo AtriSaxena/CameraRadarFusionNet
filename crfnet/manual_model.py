f71c4f0ae4a88863b6fab6f06e92e17bf65b169a
atrisaxena <atrisaxena2@gmail.com>
2023-08-10 00:36:06 +0530

    manual model

--- crfnet/manual_model.py
+++ crfnet/manual_model.py
@@ -334,13 +334,18 @@ def main():
     
     model.save('/kaggle/working/CameraRadarFusionNet/crfnet/saved_models/new_model.keras')
     
-
-    with tf.keras.backend.get_session() as sess:
-        sess.run(tf.global_variables_initializer())    
-        converter = tf.lite.TFLiteConverter.from_session(sess, model.inputs, model.outputs)
-        tflite_model = converter.convert()
-        with open("model.tflite", "wb") as f:
-            f.write(tflite_model) 
+    tf.saved_model.save(model, "tmp_model")
+    
+    
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+    print(tflite_model)
+    # with tf.keras.backend.get_session() as sess:
+    #     sess.run(tf.global_variables_initializer())    
+    #     converter = tf.lite.TFLiteConverter.from_session(sess, model.inputs, model.outputs)
+    #     tflite_model = converter.convert()
+    #     with open("model.tflite", "wb") as f:
+    #         f.write(tflite_model) 
     # # this lets the generator compute backbone layer shapes using the actual backbone model
     # if 'vgg' in cfg.network or 'densenet' in cfg.network:
     #     train_generator.compute_shapes = make_shapes_callback(model)
